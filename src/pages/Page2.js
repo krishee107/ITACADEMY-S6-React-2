2e8d672fa29b5dec55c1ea3078c64e57a925438f
Cristina Ferrer <krishee107@gmail.com>
2023-04-15 23:41:17 +0200

    Ej 6: Material UI y modal para información de los inputs

--- src/pages/Page2.js
+++ src/pages/Page2.js
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Inputs } from "../components/Inputs";
 import NavBar from "../components/Navbar";
-
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 const Page2 = () => {
+    const [open, setOpen] = useState(false);
+    const [textoModal, setTextoModal] = useState("");
+
     const [checkboxes, setCheckboxes] = useState(() => {
         const storedCheckboxes = JSON.parse(localStorage.getItem("checkboxes"));
         return storedCheckboxes ? storedCheckboxes : {
@@ -45,9 +49,6 @@ const Page2 = () => {
         })
     }
 
-
-
-
     const calculatePrice = () => {
         let newPrice = 0;
         if (checkboxes.web) {
@@ -64,6 +65,30 @@ const Page2 = () => {
             price: newPrice
         })
     }
+
+    /* MODAL */
+    const handleOpen = (texto) => {
+        setTextoModal(texto)
+        setOpen(true);
+    }
+    const handleClose = (texto) => {
+        setTextoModal(texto)
+        setOpen(false);
+    }
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        bgcolor: 'background.paper',
+        border: '3px solid #000',
+        boxShadow: 24,
+        borderRadius: '25px',
+        padding: '20px',
+        fontSize: '20px',
+        backgroundColor: 'white'
+    };
+
     return (
         <div>
             <NavBar />
@@ -73,8 +98,20 @@ const Page2 = () => {
                 <label htmlFor="web"><input type="checkbox" name="web" id="web" onChange={handleChange} checked={checkboxes.web} /> Una pàgina web (500€)</label>
                 {checkboxes.web &&
                     <div style={{ display: 'grid', border: 'solid 3px black', width: "fit-content", padding: "20px", borderRadius: "15px", gap: "10pxpm" }}>
-                        <label htmlFor="idioma">Número de páginas <Inputs value={pageInputs.paginas} name={"paginas"} onChange={handlePagesChange} /></label>
-                        <label htmlFor="idioma">Número de idiomas <Inputs value={pageInputs.idiomas} name={"idiomas"} onChange={handlePagesChange} /> </label>
+                        <label htmlFor="idioma" style={{ display: `grid` }}>
+                            Número de páginas
+                            <div style={{ display: `flex`, alignItems: `center`, gap: `20px` }}>
+                                <Inputs value={pageInputs.paginas} name={"paginas"} onChange={handlePagesChange} />
+                                <i class="fa-solid fa-circle-info" onClick={(e) => { e.preventDefault(); handleOpen("En este componente debe indicar el número de páginas que tendrá su sitio web.") }} style={{ fontSize: `25px`, color: `gray`, cursor: `pointer` }}></i>
+                            </div>
+                        </label>
+                        <label htmlFor="idioma">
+                            Número de idiomas
+                            <div style={{ display: `flex`, alignItems: `center`, gap: `20px` }}>
+                                <Inputs value={pageInputs.idiomas} name={"idiomas"} onChange={handlePagesChange} />
+                                <i class="fa-solid fa-circle-info" onClick={(e) => { e.preventDefault(); handleOpen("En este componente debe indicar el número de idiomas que tendrá su sitio web.") }} style={{ fontSize: `25px`, color: `gray`, cursor: `pointer` }}></i>
+                            </div>
+                        </label>
                     </div>
                 }
                 <label htmlFor="seo"><input type="checkbox" name="seo" id="seo" onChange={handleChange} checked={checkboxes.seo} /> Una consultoria SEO (300€)</label>
@@ -82,6 +119,17 @@ const Page2 = () => {
             </form>
 
             <span>Preu: {checkboxes.price}€</span>
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box style={style}>
+                    {textoModal}
+                </Box>
+            </Modal>
         </div>
     );
 }
