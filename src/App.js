304edb490d9dfbac526f8505caff316d0ccd28b4
Cristina Ferrer <krishee107@gmail.com>
2023-04-15 17:34:40 +0200

    Inputs de idiomas/paginas en funcion de si el checkbox web está marcado

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
     ads: false,
     price: 0
   });
+  const [pageInputs, setPageInputs] = useState({
+    paginas: 0,
+    idiomas: 0
+  })
 
   const handleChange = (event) => {
     const { name, checked } = event.target;
@@ -20,6 +24,12 @@ function App() {
       <div>¿Qué quieres hacer?</div>
       <form style={{ display: `grid`, gap: `10px`, padding: `10px 0` }}>
         <label htmlFor="web"><input type="checkbox" name="web" id="web" onChange={handleChange} /> Una pàgina web (500€)</label>
+        {checkboxes.web &&
+          <div style={{ display: 'grid' }}>
+            <label htmlFor="idioma">Número de páginas <input type="number" id="idioma" value={pageInputs.idiomas} /></label>
+            <label htmlFor="idioma">Número de idiomas <input type="number" id="paginas" value={pageInputs.paginas} /></label>
+          </div>
+        }
         <label htmlFor="seo"><input type="checkbox" name="seo" id="seo" onChange={handleChange} /> Una consultoria SEO (300€)</label>
         <label htmlFor="ads"><input type="checkbox" name="ads" id="ads" onChange={handleChange} /> Una campanya de Google Ads (200€)</label>
       </form>
