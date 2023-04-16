b78a7a58c607ce8d5213bc5e581d0fed38836db9
Cristina Ferrer <krishee107@gmail.com>
2023-04-16 14:15:22 +0200

    Modificado package.json para mostrarse en github pages

--- src/application/routes.js
+++ src/application/routes.js
@@ -3,7 +3,7 @@ import Home from '../pages/Home'
 import Page2 from '../pages/Page2';
 
 const Router = () => (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route index element={<Home />} />
             <Route path="/presupuesto/web=:webChecked?&seo=:seoChecked?&ads=:adsChecked?&pagina=:numPagina?&idioma=:numIdioma?" element={<Page2 />} />
@@ -12,7 +12,7 @@ const Router = () => (
             { /* Es muy recomendable añadir esta ruta para obtener un mensaje de error en el caso de que la ruta no exista. De lo contrario, si la ruta no existe llegaremos a una página en blanco */}
             <Route path="*" element={<div>Error 404, la página no existe.</div>} />
         </Routes>
-    </BrowserRouter>
+    </BrowserRouter >
 );
 
 export default Router;
